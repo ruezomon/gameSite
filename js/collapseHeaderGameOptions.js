75f9fabbe6a362b05ee058cba9ee5fa5f0fdfad0
ruezomon <ruezgar.guenduez@gmail.com>
2026-04-24 10:33:57 +0200

    right now, done with the header

--- js/collapseHeaderGameOptions.js
+++ js/collapseHeaderGameOptions.js
@@ -1,8 +1,11 @@
 const collapseButtonElement = document.getElementById("collapseButtonHeader");
 collapseButtonElement.addEventListener("click", function() {
     this.classList.toggle("active");
-    if (this.nextElementSibling.style.maxHeight) 
+    if (this.nextElementSibling.style.maxHeight) {
         this.nextElementSibling.style.maxHeight = null;
-    else 
+        this.nextElementSibling.style.padding = null;
+    } else {
         this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px";
+        this.nextElementSibling.style.padding = "5px 18px";
+    }
 });
