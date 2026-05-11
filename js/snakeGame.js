138cc355aec049d92decdc7906414c54f2b37fb6
ruezomon <ruezgar.guenduez@gmail.com>
2026-05-11 13:47:19 +0200

    annual update

--- js/snakeGame.js
+++ js/snakeGame.js
@@ -9,6 +9,14 @@
 // left: -1
 // up: -1
 
+const SNAKE = {
+    wall: 8,
+    head: 4,
+    snake: 2,
+    apple: 1,
+    empty: 0
+};
+
 const height = 15;
 const width = 15;
 
@@ -18,17 +26,17 @@ function buildGameBoardArray(heightInt, widthInt) {
     for (let i = 0; i < heightInt + 2; i++) {
         gameBoardArray.push([]);
         for (let j = 0; j < widthInt + 2; j++) {
-            gameBoardArray[i].push((j == 0 || j == widthInt + 1 || i == 0 || i == widthInt + 1) ? 8 : 0);
+            gameBoardArray[i].push((j == 0 || j == widthInt + 1 || i == 0 || i == widthInt + 1) ? SNAKE.wall : SNAKE.empty);
         }
     }
 }
 
-function placeHeadAt(x, y) {
-    
+function initSnakeHeadAt(x, y) {
+    gameBoardArray[x + 1][y + 1] = 4;
 }
 
 function visualizeHead() {
-    
+
 }
 
 buildGameBoardArray(height, width);
