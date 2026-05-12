93b0b6d929284f2152e3c6fb811e72dd25851c7b
ruezomon <ruezgar.guenduez@gmail.com>
2026-05-12 22:20:52 +0200

    snake

--- js/snakeGame.js
+++ js/snakeGame.js
@@ -1,14 +1,3 @@
-// head: 8
-// snake-parts: 4
-// walls: 2
-// apples: 1
-// empty: 0
-
-// down: +1
-// right: +1
-// left: -1
-// up: -1
-
 const SNAKE = {
     wall: 8,
     head: 4,
@@ -17,12 +6,50 @@ const SNAKE = {
     empty: 0
 };
 
+const DIRECTION = {
+    up: 0,
+    right: 1,
+    down: 2,
+    left: 3
+};
+
+class Snake {
+    headX;
+    headY;
+    length;
+
+    constructor(x, y, length) {
+        this.length = length;
+        this.headX = x;
+        this.headY = y;
+        
+        gameBoardArray[y + 1][x + 1] = SNAKE.head;
+        for (let i = 0; i < length - 1; i++)
+            gameBoardArray[y + 1][x - i] = SNAKE.snake;
+    }
+    moveUp() {
+
+    }
+    moveDown() {
+
+    }
+    moveRight() {
+
+    }
+    moveLeft() {
+
+    }
+};
+
 const height = 15;
 const width = 15;
 
 const gameBoardArray = [];
+const gameBoardElement = document.getElementById("snake-game-board");
 
 function buildGameBoardArray(heightInt, widthInt) {
+    while (true)
+        if (gameBoardArray.pop() === undefined) break;
     for (let i = 0; i < heightInt + 2; i++) {
         gameBoardArray.push([]);
         for (let j = 0; j < widthInt + 2; j++) {
@@ -31,12 +58,22 @@ function buildGameBoardArray(heightInt, widthInt) {
     }
 }
 
-function initSnakeHeadAt(x, y) {
-    gameBoardArray[x + 1][y + 1] = 4;
-}
-
-function visualizeHead() {
-
+function update() {
+    gameBoardArray.forEach((element, y) => {
+        if (y === 0 || y === height + 1) return;
+        element.forEach((element, x) => {
+            if (element === 8) return;
+            gameBoardElement[y + 1][x + 1] = element === SNAKE.head ? "head" : 
+                                                element === SNAKE.snake ? "tail" : 
+                                                element === SNAKE.apple ? "apple" : "none";
+        });
+    });
 }
 
 buildGameBoardArray(height, width);
+player = new Snake(5, 3, 3);
+gameBoardArray.forEach(element => {
+    console.log(element);
+});
+
+update();
