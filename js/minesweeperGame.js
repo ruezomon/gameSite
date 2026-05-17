2af7f2bc0aeab806f16ca9688812a5dbaf853f15
ruezomon <ruezgar.guenduez@gmail.com>
2026-05-17 20:07:33 +0200

    started minesweeper
    annual update

--- js/minesweeperGame.js
+++ js/minesweeperGame.js
@@ -5,35 +5,46 @@ const gameBoardElement = document.getElementsByClassName("minesweeper-gameboard"
 const tileTemplate = `<div class="minesweeper-tile"><button class="minesweeper-clickable"></button></div>`;
 const rowTemplate = `<div class="minesweeper-row"></div>`;
 
-let bombSum = Number();
-let bombsLeft = Number();
-let bombsMarked = Number();
-let wrongBombsMarked = Number();
+let boardWidth = 9;
+let boardHeight = 9;
+let bombSum = 10;
+let bombsMarkedSum = 0; 
+let wrongBombsMarked = 0;
+let gameActive = true;
+let started = false;
 
 difficultyToggleButton.addEventListener("click", () => {
-    reset();
     if (difficultyToggleButton.innerHTML === "Easy") {
         difficulty = 1;
         difficultyToggleButton.innerHTML = "Normal";
-        clearGameBoard();
-        buildGameBoard(16, 16);
+        bombSum = 40;
+        boardHeight = 16;
+        boardWidth = 16;
     } else if (difficultyToggleButton.innerHTML === "Normal") {
         difficulty = 2;
         difficultyToggleButton.innerHTML = "Hard";
-        clearGameBoard();
-        buildGameBoard(16, 30);
+        bombSum = 99;
+        boardHeight = 16;
+        boardWidth = 30;
     } else {
         difficulty = 0;
         difficultyToggleButton.innerHTML = "Easy";
-        clearGameBoard();
-        buildGameBoard(9, 9);
+        bombSum = 10;
+        boardHeight = 9;
+        boardWidth = 9;
     }
+    reset();
 });
 
 document.getElementById("minesweeper-reset").addEventListener("click", reset);
 
 function reset() {
-
+    bombsLeft = bombSum;
+    bombsMarkedSum = 0;
+    clearGameBoard();
+    buildGameBoard(boardHeight, boardWidth);
+    makeDiscoverable();
+    gameActive = true;
 }
 
 function clearGameBoard() {
@@ -49,22 +60,51 @@ function buildGameBoard(rows, collumns) {
     }
 }
 
-function placeBombs() {
-    for (let i = 0; i < difficulty === 0 ? 10 : difficulty === 1 ? 40 : 99; i++) {
+function placeBombs(startingRow, startingCollumn) {
+    for (let i = 0; i < bombSum; i++) {
         let c = Number();
         let r = Number();
-        console.log(gameBoardElement.children[0]);
-        /*
-        [].slice.call(gameBoardElement.children).forEach((e) => {
-            console.log(e);
-        });
-        
-        /*
         do {
-            c = Math.round((Math.random() * 100) % difficulty === 0 ? 9 : difficulty === 1 ? 16 : 30);
-            r = Math.round((Math.random() * 100) % difficulty >= 1 ? 16 : 9);
-        } while (!gameBoardElement.children[r].children[c].children[0].classList.contains("bomb"));
+            c = Math.round(Math.random() * 100) % boardWidth;
+            r = Math.round(Math.random() * 100) % boardHeight;
+        } while (gameBoardElement.children[r].children[c].children[0].classList.contains("bomb") || (r === startingRow && c === startingCollumn));
+
         gameBoardElement.children[r].children[c].children[0].classList.add("bomb");
-        */
+        gameBoardElement.children[r].children[c].classList.add("bomb-parent");
     }
 }
+
+function makeDiscoverable() {
+    for (let i = 0; i < boardHeight; i++) {
+        for (let j = 0; j < boardWidth; j++) {
+
+            gameBoardElement.children[i].children[j].children[0].addEventListener("click", (event) => {
+                if (!gameActive) return;
+                if (gameBoardElement.children[i].children[j].children[0].classList.contains("marked-as-bomb")) return;
+                gameBoardElement.children[i].children[j].children[0].remove();
+            });
+
+            gameBoardElement.children[i].children[j].children[0].addEventListener("contextmenu", (event) => {
+                if (!gameActive) return;
+                event.preventDefault();
+                if (gameBoardElement.children[i].children[j].children[0].classList.contains("marked-as-bomb")) {
+                    bombsMarkedSum--;
+                    if (!gameBoardElement.children[i].children[j].classList.contains("bomb-parent")) wrongBombsMarked--;
+                } else {
+                    bombsMarkedSum++;
+                    if (!gameBoardElement.children[i].children[j].classList.contains("bomb-parent")) wrongBombsMarked++;
+                }
+                gameBoardElement.children[i].children[j].children[0].classList.toggle("marked-as-bomb");
+            });
+
+        }
+    }
+}
+
+function start() {
+    placeBombs();
+}
+
+buildGameBoard(9, 9);
+placeBombs();
+makeDiscoverable();
