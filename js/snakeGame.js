f32a12624c5448f569a81c952e81fdb6fadd1232
ruezomon <ruezgar.guenduez@gmail.com>
2026-06-03 09:17:29 +0200

    final turn-in

--- js/snakeGame.js
+++ js/snakeGame.js
@@ -166,7 +166,7 @@ class gameBoard {
 
     buildGameBoardArray() {
         while (true) 
-            if (gameBoardArray.pop() === undefined) break;;
+            if (gameBoardArray.pop() === undefined) break;
         for (let i = 0; i < this.height + 2; i++) {
             gameBoardArray.push([]);
             for (let j = 0; j < this.width + 2; j++) {
